430da0dcf08397e0ddc24255f19aac6b18a859fc
VicoAdli <vicoadli@gmail.com>
2023-11-15 19:51:27 +0700

    Progress Tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 import datetime
+import json
 from django.urls import reverse
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
@@ -126,3 +127,22 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
